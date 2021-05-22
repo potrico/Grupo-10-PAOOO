8875c98ad9b3c78e77352cc17affac95af5317c7
potrico <bruno.potrich@gmail.com>
2021-05-22 17:41:04 -0300

    Commit Inicial

---
+++ ProjetoFrotas/src/main/java/br/upf/ads/ProjetoFrotas/App.java
@@ -0,0 +1,13 @@
+package br.upf.ads.ProjetoFrotas;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
